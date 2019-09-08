cbee54c4c3d7c7c4a140f4cb5289e81d63676cd2
Vanadiiii <ivan.matveev94@mail.ru>
2019-09-09 00:15:58 +0400

    First commit

---
+++ src/main/java/ru/dexsys/DBStartButton.java
@@ -0,0 +1,8 @@
+package ru.dexsys;
+
+public class DBStartButton {
+    public static void main(String[] args) {
+        DBTest app = new DBTest();
+        app.run();
+    }
+}
---
+++ src/main/java/ru/dexsys/DBTest.java
@@ -0,0 +1,65 @@
+package ru.dexsys;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+class DBTest { //Вначале задаю драйвер для подключения к mySQL, URL, логин и пароль, чтоб не растягивать всё в большую "портянку")
+    private static final String DRIVERNAME = "com.mysql.cj.jdbc.Driver";
+    private static final String TIMEZONE = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://db4free.net:3306/dexautomation" + TIMEZONE;
+    //После знака вопроса в строке выше я указываю часовой пояс и его параметры, чтоб исключить все ошибки с ним.
+    private static final String USERNAME = "dexautomation";
+    private static final String PASSWORD = "dexautomation";
+
+    void run() { //в методе run сразу описываю ВСЕ исключения для подключения)
+        try { //проверка драйвера для mySQL
+            Class.forName(DRIVERNAME);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Can't get class. No driver found");
+            e.printStackTrace();
+            return;
+        }
+        Connection connection;
+        try { //подключаюсь к БД
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Can't get connection. Incorrect URL");
+            e.printStackTrace();
+            return;
+        }
+
+
+        /*Вот сюда буду писать все запросы к БД!*/
+        try { //создаю объект Statement'а для подключения к БД, прописываю данные
+            Statement statement = connection.createStatement();
+            String sqlCommandAddStudent = "INSERT INTO Students(firstName, lastName, age, phone) VALUES ('Ivan', 'Matveev', 24, 89127684213);";
+            String sqlCommandDeletStudent = "DELETE from dexautomation where (firstName = 'Ivan' and lastName = 'Matveev')";
+            String sqlCommandValidation = "SELECT * from Students where (firstName = 'Ivan' and lastName = 'Matveev' and age = 24 and phone = 89127684213)";
+//            statement.execute(sqlCommandAddStudent);
+//            statement.execute(sqlCommandDeletStudent);
+            String validation = statement.executeQuery(sqlCommandValidation).toString();
+            Assert.assertEquals(validation, "22222");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } /*А здесь все запросы заканчиваются) */
+
+        try { //проверка закрытия подключения
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Can't close connection");
+            e.printStackTrace(); // отличается от toString() тем, что вызывает весь стек, что полезно для отладки.
+        }
+    }
+
+    @Test
+    public static void main(String[] args) {
+        DBTest app = new DBTest();
+        app.run();
+    }
+}
+
